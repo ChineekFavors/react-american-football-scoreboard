9af76a678b8df04cce4118f06d617be1fdb42657
Chineek Favors <chineekfavors01@gmail.com>
2019-07-24 21:01:17 -0700

    added state setter functions to click listeners to buttons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, homeChangeScore] = useState(32)
 const [awayScore, awayChangeScore] = useState(32)
-console.log(useState);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -30,12 +29,12 @@ console.log(useState);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={ () => homeChangeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={ () => homeChangeScore(homeScore + 3)}className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={ () => awayChangeScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={ () => awayChangeScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
